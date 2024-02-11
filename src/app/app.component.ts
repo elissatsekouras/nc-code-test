d2c12f704a85e82c9e770804490084f08da377ef
Princess Elissa <tsekouras.elissa@gmail.com>
2024-02-11 12:07:27 +0100

    Create working countdown app with Elissas birthday as default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventNameComponent } from './components/event-name/event-name.component';
+import { CountdownTimerComponent } from './components/countdown-timer/countdown-timer.component';
+import { InputComponent } from './components/input/input.component';
+import { Event } from './types/event.type';
+import { HelperService } from './services/helper.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    EventNameComponent,
+    CountdownTimerComponent,
+    InputComponent,
+  ],
+  providers: [HelperService],
   template: `
-    <div></div>
+    <main>
+      <section class="countdown-app">
+        <app-event-name [title]="event.name" />
+        <app-countdown-timer [dDay]="event.date" />
+        <div class="input-container">
+          <app-input
+            label="Title"
+            type="text"
+            placeholder="Name your event"
+            [value]="event.name"
+            [onChange]="handleTitleChange.bind(this)"
+          />
+          <app-input
+            label="Date"
+            type="date"
+            value="{{ helpers.formatDate(this.event.date) }}"
+            [onChange]="handleDateChange.bind(this)"
+          />
+        </div>
+      </section>
+    </main>
   `,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'event-countdown-app';
+  constructor(
+    public helpers: HelperService,
+  ) {}
+
+  event: Event = {
+    name: "Elissa's 30th BDAY 🎉",
+    date: new Date('2024-02-13'),
+  };
+
+  handleDateChange(e: any) {
+    this.event.date = new Date(e.target.value);
+  }
+
+  handleTitleChange(e: any) {
+    this.event.name = e.target.value;
+  }
 }
